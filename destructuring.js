fb708135e1f00205dd8cb8be6efc49b1b6eee49e
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-07-06 20:43:16 +0200

    Opdrachten destructuring gemaakt, snap het nog niet echt

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,11 @@ const product = {
   screenType: 'LED',
 }
 
+const {price, refreshRate} = product;
+
+console.log({price});
+console.log({ refreshRate});
+
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -17,6 +22,10 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+const {general: {brand, type} } = productInformation;
+console.log({brand});
+console.log({type});
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -38,27 +47,45 @@ const tvOptions = {
   },
 };
 
+const {options: {connectivity: { popular: {wifi, bluetooth }}}} = tvOptions;
+console.log({wifi});
+console.log({bluetooth});
+
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
+const [first, second] = connectivity;
 
+console.log({first});
+console.log({second});
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
 
+const [eerste,, derde] = tvSizes;
+console.log({eerste});
+console.log({derde});
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
 
 function getCompanyDetails() {
   return {
-    name: 'Novi Hogeschool',
+    name: `Novi Hogeschool`,
     adress: 'Zonnebaan 9, Utrecht',
   }
 }
+
+const {name, adress} = getCompanyDetails();
+console.log({name});
+console.log({adress});
 
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+const [firstDetail, secondDetail] = getDetails("Sebas", 33);
+console.log({firstDetail});
+console.log({secondDetail});
